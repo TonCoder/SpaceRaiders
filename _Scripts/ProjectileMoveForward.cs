04804b159e6e6e12afa86af816b0396976e09f61
Tony J <jxje7166@email.cpcc.edu>
2019-06-27 19:49:25 -0400

    start

---
+++ _Scripts/ProjectileMoveForward.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMoveForward : MonoBehaviour {
+
+
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float despawnTimer = 2;
+
+    void Start()
+    {
+        StartCoroutine(DespawnObject());
+    }
+
+    public void SetSpeed(float value)
+    {
+        speed = value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveVelocity = speed * Time.deltaTime * transform.forward;
+        transform.position += moveVelocity;
+    }
+
+    IEnumerator DespawnObject()
+    {
+        yield return new WaitForSeconds(despawnTimer);
+        //SimplePoolManager.instance.DisablePoolObject (transform);
+        Destroy(gameObject);
+    }
+}
